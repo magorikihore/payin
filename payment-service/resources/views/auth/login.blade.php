a803be905abe1dc1d4fef7bc9bc2942939c3a3f0
magorikihore <magorikihore@users.noreply.github.com>
2026-02-28 02:41:10 -0800

    Change login header to LOGIN

--- payment-service/resources/views/auth/login.blade.php
+++ payment-service/resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
                 </svg>
             </div>
-            <h2 class="text-2xl font-bold text-gray-800">Payin</h2>
+            <h2 class="text-2xl font-bold text-gray-800">LOGIN</h2>
             <p class="text-gray-500 mt-1">Login to your account</p>
         </div>
 
